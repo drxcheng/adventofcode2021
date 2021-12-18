bdc6df946f5fcf42c87cfcb55a8e88d19a2b6d9b
Xu Cheng <xu@drxcheng.com>
2021-12-17 21:33:52 -0800

    Dec 16th - try again - part TWO ran but answer not correct

--- 16.go
+++ 16.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    // "os"
-    // "bufio"
+    "os"
+    "bufio"
     "strconv"
     "strings"
     "fmt"
@@ -77,94 +77,97 @@ func getValue(values []int, operation int64) int {
             return 0
         }
     } else {
+        fmt.Println(operation)
         panic("wrong operation")
     }
 }
 
-func readPacket(bitString *[]rune, operation int64) int {
+func readPacket(bitString *[]rune, onlyOne bool) []int {
     if (len(*bitString) <= 6) {
-        // fmt.Println("End of all packets")
-        return 0
+        fmt.Println("End of all packets")
+        return []int{}
     }
 
-    // fmt.Println("Read packet: ", string(bitString))
+    fmt.Println("Read packet:", string(*bitString))
     version, _ := strconv.ParseInt(string((*bitString)[0:3]), 2, 64)
     *bitString = (*bitString)[3:]
-    // fmt.Println("packet version:", version)
+    fmt.Println("packet version:", version)
     versionNumberSum += int(version)
     // 3 bits T
     typeId, _ := strconv.ParseInt(string((*bitString)[0:3]), 2, 64)
     *bitString = (*bitString)[3:]
-    // fmt.Println("typeId:", typeId)
+    fmt.Println("typeId:", typeId)
 
     values := []int{}
     if typeId == 4 {
         // literal
         for len(*bitString) >= 5 {
+            literalNumber, _ := strconv.ParseInt(string((*bitString)[1:5]), 2, 64)
+            values = append(values, int(literalNumber))
             if (*bitString)[0] == '0' {
-                literalNumber, _ := strconv.ParseInt(string((*bitString)[1:5]), 2, 64)
-                values = append(values, int(literalNumber))
                 // end of a packet
-                allZero := true
-                for _, char := range (*bitString)[5:] {
-                    if char == '1' {
-                        allZero = false
-                        break
-                    }
-                }
-                if !allZero {
-                    subPacketString := (*bitString)[5:]
-                    packetResult := readPacket(&subPacketString, -1)
-                    values = append(values, packetResult)
-                } else {
-                    break
-                }
+                *bitString = (*bitString)[5:]
+                fmt.Println("End of literal. Values:", values)
+                break
             } else {
-                literalNumber, _ := strconv.ParseInt(string((*bitString)[1:5]), 2, 64)
-                values = append(values, int(literalNumber))
                 *bitString = (*bitString)[5:]
             }
         }
+
+        fmt.Println("End of all literal. Values:", values)
     } else {
         // operation
         lengthTypeId := (*bitString)[0]
+        fmt.Println("lengthTypeId:", string(lengthTypeId))
         *bitString = (*bitString)[1:]
 
         if lengthTypeId == '0' {
             // next 15
             length, _ := strconv.ParseInt(string((*bitString)[0:15]), 2, 64)
-            // fmt.Println("length:", length)
+            fmt.Println("length:", length)
             *bitString = (*bitString)[15:]
 
             subPacketString := (*bitString)[:length]
-            packetResult := readPacket(&subPacketString, typeId)
-            values = append(values, packetResult)
+            subPacketValues := readPacket(&subPacketString, false)
+            *bitString = (*bitString)[length:]
 
-            if len(*bitString) > 0 {
-                packetResult := readPacket(bitString, typeId)
-                values = append(values, packetResult)
-            }
+            fmt.Println("end of the type 0 sub-packets:", subPacketValues)
+
+            values = append(values, getValue(subPacketValues, typeId))
         } else {
             // next 11
             count, _ := strconv.ParseInt(string((*bitString)[0:11]), 2, 64)
-            // fmt.Println("count:", count)
+            fmt.Println("count:", count)
             *bitString = (*bitString)[11:]
+            subPacketValues := []int{}
             for i := 0; i < int(count); i++ {
-                packetResult := readPacket(bitString, typeId)
-                values = append(values, packetResult)
+                packetResult := readPacket(bitString, true)
+                for _, v := range packetResult {
+                    subPacketValues = append(subPacketValues, v)
+                }
             }
+            fmt.Println("end of the type 1 sub-packets:", subPacketValues)
+
+            values = append(values, getValue(subPacketValues, typeId))
         }
     }
 
-    return getValue(values, operation)
+    if !onlyOne && len(*bitString) >= 11 {
+        subsequenceResults := readPacket(bitString, false)
+        for _, v := range subsequenceResults {
+            values = append(values, v)
+        }
+    }
+
+    return values
 }
 
 func main() {
-    // file, _ := os.Open("16.txt")
-    // defer file.Close()
-    // scanner := bufio.NewScanner(file)
-    // scanner.Scan()
-    // hexString := scanner.Text()
+    file, _ := os.Open("16.txt")
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    scanner.Scan()
+    hexString := scanner.Text()
     // hexString := "D2FE28"
     // hexString := "38006F45291200"
     // hexString := "EE00D40C823060"
@@ -172,14 +175,14 @@ func main() {
     // hexString := "620080001611562C8802118E34"
     // hexString := "C0015000016115A2E0802F182340"
     // hexString := "A0016C880162017C3686B18A3D4780"
-    hexString := "C200B40A82" // [-8 1 2]
-    // hexString := "04005AC33890" // [-7 6 9]
-    // hexString := "880086C3E88112" // [-6 7 8 9]
-    // hexString := "CE00C43D881120" // [-5 7 8 9]
-    // hexString := "D8005AC2A8F0" //  [-2 5 15]
-    // hexString := "F600BC2D8F" // [-3 5 15]
-    // hexString := "9C005AC2F8F0" // [-1 5 15]
-    // hexString := "9C0141080250320F1802104A08" // [-1 -8 1 3 -7 2 2]
+    // hexString := "C200B40A82" // [+ 1 2]
+    // hexString := "04005AC33890" // [* 6 9]
+    // hexString := "880086C3E88112" // [min 7 8 9]
+    // hexString := "CE00C43D881120" // [max 7 8 9]
+    // hexString := "D8005AC2A8F0" //  [< 5 15]
+    // hexString := "F600BC2D8F" // [> 5 15]
+    // hexString := "9C005AC2F8F0" // [= 5 15]
+    // hexString := "9C0141080250320F1802104A08" // [= (+ 1 3) (* 2 2)]
 
     bitString := []rune{}
     for _, v := range strings.Split(hexString, "") {
@@ -187,10 +190,10 @@ func main() {
             bitString = append(bitString, char)
         }
     }
-    // fmt.Println(string(bitString))
+    fmt.Println(string(bitString))
 
-    result := readPacket(&bitString, -1)
+    result := readPacket(&bitString, false)
 
     fmt.Println("versionNumberSum:", versionNumberSum)
-    fmt.Println("result:", result)
+    fmt.Println("result:", result[0])
 }
